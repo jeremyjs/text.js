cc33e8e372b5fc9ce31427b0fbfb9ea78c406e0c
Jeremy Meyer <jjman505@gmail.com>
2014-11-26 18:51:14 -0500

    it's working!!!

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,47 @@
+
 $(function() {
-  var realConsoleLog = console.log;
-  console.log = function () {
-    var message = [].join.call(arguments, " ");
-    // Display the message somewhere... (jQuery example)
-    $("#console").text(message);
-    realConsoleLog.apply(console, arguments);
+
+  function userOutput(msg) {
+    $('#output').append('<p class="terminal-line user-input"> > ' + msg + '</p>');
   };
 
-  console.log("hello", "my", "name", "is", "james");
+  function gameOutput(msg) {
+    $('#output').append('<p class="terminal-line game-output">' + msg + '</p>');
+  }
 
+  var maxLines = 20;
+  box = false;
+
+  var log = console.log;
+  console.log = function () {
+    var message = [].join.call(arguments, " ");
+    userOutput(message);
+    lines++;
+    if(lines > maxLines) $('#output').children().first().remove();
+    interpret(message);
+    log.apply(console, arguments);
+  };
+
+  function interpret(cmd) {
+    var msg = "I don't know that command!";
+    if(cmd === 'use box') {
+      if(box) {
+        msg = 'You hide in the box.';
+      } else {
+        msg = "You don't have a box!";
+      }
+    }
+    gameOutput(msg);
+  }
+
+  var lines = 0;
+
+  var $in = $('#input');
+  $in.keyup(function(e){
+    if(e.which === 13) {
+      var cmd = $(this).val();
+      console.log(cmd);
+    }
+    e.preventDefault();
+  });
 });
